75fab93b463ae3f330e7e1dd6ff19d45dedb6ce2
LeonMesquita <leonmesquita16@gmail.com>
2022-08-02 16:52:33 -0300

    .

---
+++ src/controllers/authController.js
@@ -0,0 +1,13 @@
+
+
+
+export async function registerUser(req, res){
+
+}
+
+
+
+
+export async function loginUser(req, res){
+    
+}
---
+++ src/routes/authRouter.js
@@ -0,0 +1,5 @@
+import {Router} from   'express';
+const router = Router();
+import { registerUser, loginUser } from '../controllers/authController.js';
+
+export default router;
