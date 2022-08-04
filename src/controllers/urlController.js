ff0773b67c99ba9d55c5247d29abbe79060be064
LeonMesquita <leonmesquita16@gmail.com>
2022-08-03 21:16:32 -0300

    redirecionar url

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -34,3 +34,29 @@ export async function getUrlById(req, res){
         return res.sendStatus(500);
     }
 }
+
+
+export async function openShortUrl(req, res){
+    const shortUrl = req.params.shortUrl;
+    try{
+        const {rows: url} = await connection.query(`
+            SELECT * FROM urls WHERE "shortUrl" = $1
+        `, [shortUrl]);
+        if(url.length === 0){
+            return res.sendStatus(404);
+        }
+        const visitorsCount = url[0].visitCount + 1;
+        await connection.query(`
+            UPDATE urls SET "visitCount" = $1 WHERE id = $2
+        `, [visitorsCount, url[0].id]);
+        res.redirect(url[0].url);
+    }catch(error){
+        return res.sendStatus(500);
+    }
+
+}
+
+
+export async function deleteUrl(req, res){
+
+}
--- src/routes/urlRouter.js
+++ src/routes/urlRouter.js
@@ -1,10 +1,12 @@
 import {Router} from   'express';
 const router = Router();
-import {shortenUrl, getUrlById} from '../controllers/urlController.js';
+import {shortenUrl, getUrlById, openShortUrl, deleteUrl} from '../controllers/urlController.js';
 import {validateUrl} from '../middlewares/validateUrl.js';
 import {validateUser} from '../middlewares/validateUser.js'
 
 
 router.post('/urls/shorten',validateUser, validateUrl, shortenUrl);
 router.get('/urls/:id', getUrlById);
+router.get('/urls/open/:shortUrl', openShortUrl);
+router.delete('/urls/:id', deleteUrl);
 export default router;
